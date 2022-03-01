2f6028a2023ad99ac2031ceb9511a8eb4b754ea6
ZinCOre2 <98097347+ZinCOre2@users.noreply.github.com>
2022-03-01 13:13:38 +0300

    Dot Threshold Update #2

--- Assets/Scripts/DotThreshold.cs
+++ Assets/Scripts/DotThreshold.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 public class DotThreshold : MonoBehaviour
 {
     [SerializeField] private Transform targetTransform;
+    [Range(0f, 180f)]
     [SerializeField] private float angleThreshold;
 
     private void OnDrawGizmos()
     {
-        var vectorToTarget = targetTransform.position - transform.position;
-        var lookAtVector = transform.right;
+        if (!targetTransform) { return; }
 
-        var dotProduct = vectorToTarget.x * lookAtVector.x + vectorToTarget.y * lookAtVector.y + vectorToTarget.z * lookAtVector.z;
-        var angle = Mathf.Acos(dotProduct / (vectorToTarget.magnitude * lookAtVector.magnitude)) * Mathf.Rad2Deg;
+        var watcherPosition = transform.position;
+        var vectorToTarget = (targetTransform.position - watcherPosition).normalized;
+        var lookVector = transform.right;
+
+        // Vector3.Dot
+        var dotProduct = vectorToTarget.x * lookVector.x + vectorToTarget.y * lookVector.y + vectorToTarget.z * lookVector.z;
+        // var angle = Mathf.Acos(dotProduct / (vectorToTarget.magnitude * lookAtVector.magnitude)) * Mathf.Rad2Deg;
+        var angle = Mathf.Acos(dotProduct) * Mathf.Rad2Deg;
         
         var isInThreshold = Mathf.Abs(angle) < Mathf.Abs(angleThreshold) ? true : false;
         Gizmos.color = isInThreshold ? Color.green : Color.red; 
 
-        Gizmos.DrawRay(transform.position, lookAtVector);
+        Gizmos.DrawRay(watcherPosition, lookVector);
     }
 }
