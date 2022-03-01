21c1ba76203f01b16fa50d4fddc1aa41356108f1
ZinCOre2 <98097347+ZinCOre2@users.noreply.github.com>
2022-03-01 13:23:58 +0300

    Dot Threshold Update 3 (uses cos threshold)

--- Assets/Scripts/DotThreshold.cs
+++ Assets/Scripts/DotThreshold.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 public class DotThreshold : MonoBehaviour
 {
     [SerializeField] private Transform targetTransform;
+    [Range(-1f, 1f)] 
+    [SerializeField] private float threshold;
+    [SerializeField] private bool angleThresholdOverride;
     [Range(0f, 180f)]
     [SerializeField] private float angleThreshold;
 
+    private bool _isInThreshold;
+    
     private void OnDrawGizmos()
     {
         if (!targetTransform) { return; }
@@ -15,13 +20,22 @@ public class DotThreshold : MonoBehaviour
         var vectorToTarget = (targetTransform.position - watcherPosition).normalized;
         var lookVector = transform.right;
 
-        // Vector3.Dot
-        var dotProduct = vectorToTarget.x * lookVector.x + vectorToTarget.y * lookVector.y + vectorToTarget.z * lookVector.z;
-        // var angle = Mathf.Acos(dotProduct / (vectorToTarget.magnitude * lookAtVector.magnitude)) * Mathf.Rad2Deg;
-        var angle = Mathf.Acos(dotProduct) * Mathf.Rad2Deg;
-        
-        var isInThreshold = Mathf.Abs(angle) < Mathf.Abs(angleThreshold) ? true : false;
-        Gizmos.color = isInThreshold ? Color.green : Color.red; 
+        if (angleThresholdOverride)
+        {
+            // Vector3.Dot
+            var dotProduct = vectorToTarget.x * lookVector.x + vectorToTarget.y * lookVector.y;
+            // var angle = Mathf.Acos(dotProduct / (vectorToTarget.magnitude * lookAtVector.magnitude)) * Mathf.Rad2Deg;
+            var angle = Mathf.Acos(dotProduct) * Mathf.Rad2Deg;
+
+            _isInThreshold = Mathf.Abs(angle) <= Mathf.Abs(angleThreshold) ? true : false;
+        }
+        else
+        {
+            var dotProduct = vectorToTarget.x * lookVector.x + vectorToTarget.y * lookVector.y;
+            _isInThreshold = dotProduct >= threshold ? true : false;
+        }
+
+        Gizmos.color = _isInThreshold ? Color.green : Color.red; 
 
         Gizmos.DrawRay(watcherPosition, lookVector);
     }
